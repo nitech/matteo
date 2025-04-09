b4316d41efc00c95092718f533bcd6e8b8e83c0d
Simon Pedersen <nitech@gmail.com>
2025-04-09 20:39:44 +0200

    DIverse

--- obj/Debug/net8.0/Matteo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Matteo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Matteo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd60892fda3cf5c0541d6e108ca86e71310a3286")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b638775fc9d155c4f33bfb72f01589d5808a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Matteo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Matteo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
